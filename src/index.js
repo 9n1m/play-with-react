4efc38c3fce898990767a1aefd9b1d68483ac694
wgywmm <wgywmm@gmail.com>
2022-03-14 14:00:23 +0800

    JSX Represents Objects

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const user = {
-  avatarUrl: 'https://reactjs.org/logo-180x180.png'
-}
+// These two examples are identical
+const element1 = (
+  <h1 className="greeting">
+    Hello, world!
+  </h1>
+);
 
-// If a tag is empty, you may close it immediately with />, like XML
-const element1 = <img src={user.avatarUrl} />;
-
-// JSX tags may contain children
-const element2 = (
-  <div>
-    <h1>Hello!</h1>
-    <h2>Good to see you here.</h2>
-  </div>
+const element2 = React.createElement(
+  'h1',
+  { className: 'greeting' },
+  'Hello, world!'
 );
 
 const element = (
-  // uses camelCase property naming convention instead of HTML attribute names
-  <div className='ele' tabIndex={1}>
+  <div>
     {element1}
     {element2}
   </div>
