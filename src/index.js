e94639b773656085daa1124bae9a7890a23b9e37
wgywmm <wgywmm@gmail.com>
2022-03-14 14:27:23 +0800

    User-Defined Components Must Be Capitalized

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const MyComponents = {
-  DatePicker: function DatePicker(props) {
-    return <div>Imagine a {props.color} datepicker here.</div>;
-  }
+// Wrong! This is a component and should have been capitalized:
+function hello(props) {
+  // Correct! This use of <div> is legitimate because div is a valid HTML tag:
+  return <div>Hello {props.toWhat}</div>;
 }
 
-function BlueDatePicker() {
-  // if MyComponents.DatePicker is a component, you can use it directly from JSX
-  return <MyComponents.DatePicker color="blue" />;
+function HelloWorld1() {
+  // Wrong! React thinks <hello /> is an HTML tag because it's not capitalized:
+  return <hello toWhat="World" />;
+}
+
+// ---------------------------------
+
+// Correct! This is a component and should be capitalized:
+function Hello(props) {
+  // Correct! This use of <div> is legitimate because div is a valid HTML tag:
+  return <div>Hello {props.toWhat}</div>;
+}
+
+function HelloWorld2() {
+  // Correct! React knows <Hello /> is a component because it's capitalized.
+  return <Hello toWhat="World" />;
 }
 
 const element = (
-  <BlueDatePicker />
+  <>
+    <HelloWorld1 />
+    <HelloWorld2 />
+  </>
 )
 
 ReactDOM.render(
